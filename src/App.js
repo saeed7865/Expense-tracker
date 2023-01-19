bf6d65534ee475299f51639e895d6f2ac7725b9f
saeed7865 <107575606+saeed7865@users.noreply.github.com>
2023-01-19 10:48:20 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './Components/InputFields'
+import InputFields from './Components/InputFields';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <InputFields />
+    </div>
+  );
+}
+
+export default App;
